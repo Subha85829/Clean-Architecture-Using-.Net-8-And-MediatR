a3fee868eb5146f1d9287c3652f847211302fc35
Subha85829 <subhasishmodak27@gmail.com>
2025-03-22 12:28:31 +0530

    Add project files.

---
+++ MyCleanArchitectureApp.Api/src/MyCleanArchitectureApp.Api/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MyCleanArchitectureApp.Application;
+using MyCleanArchitectureApp.Infrastructure;
+
+namespace MyCleanArchitectureApp.Api
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddAppDI(this IServiceCollection services)
+		{
+			services.AddApplicationDI()
+					.AddInfraDI();
+			return services;
+		}
+	}
+}
